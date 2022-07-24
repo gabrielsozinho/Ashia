f8ac3195c9c10bac5c6f19412aed6699173e6d25
Gabriel Sozinho <90806505+gabrielsozinho@users.noreply.github.com>
2022-07-24 16:33:49 +0000

    .

---
+++ replit.nix
@@ -0,0 +1,22 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python39Packages.bootstrapped-pip
+pkgs.python39Packages.pip
+pkgs.python39Packages.pip
+pkgs.import os
+pkgs.python38Full
+  ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      # Needed for pandas / numpy
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      # Needed for pygame
+      pkgs.glib
+      # Needed for matplotlib
+      pkgs.xorg.libX11
+    ];
+    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
+    LANG = "en_US.UTF-8";
+  };
+}
